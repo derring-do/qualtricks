b3c74f4100db7e389e0509b2be4168c422f6ce85
derring-do <5824530+derring-do@users.noreply.github.com>
2018-06-06 18:10:28 -0400

    eod

--- R/previewIntercept.R
+++ R/previewIntercept.R
@@ -1,18 +1,63 @@
 #' Start a selenium server and browser with or without Docker
 #'
-#' @return Opens empty browser window asdf
+#' @return remDr
+#' @import RSelenium
 #' @export
 #'
 #' @examples
 startSelServer <- function(browser = "chrome", useDocker = FALSE) {
   if(useDocker == TRUE) {
+    userDocker <<- TRUE  # for later stop
     shell("docker run --rm -d -p 4445:4444 selenium/standalone-chrome")
+    Sys.sleep(1)
     docker.container.id <- gsub(" ", "", strsplit(shell("docker ps -a", intern = TRUE)[2], "selenium")[[1]][1])
-    remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "chrome")
+    remDr <<- remoteDriver(remoteServerAddr = "localhost", port = 4445L, browserName = "chrome")
+    Sys.sleep(1)
     remDr$open()
   } else {
     rD <- rsDriver(browser = c(browser))
-    remDr <- rD[['client']]
+    remDr <<- rD[['client']]
   }
 }
+
+#' Title
+#'
+#' @return
+#' @export
+#'
+#' @examples
+stopSelServer <- function(userDocker = useDocker) {
+  if(useDocker == TRUE) {
+    remDr$close()
+    shell(paste("docker kill", gsub(" ", "", strsplit(shell("docker ps -a", intern = TRUE)[2], "selenium")[[1]][1])))
+  } else {
+    rD$server$stop()
+  }
+}
+
+#' Format JavaScript bookmarklet that triggers Qualtrics Intercept Edit Preview
+#'
+#' @param interceptId
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+formatInterceptBookmarklet <- function(interceptId, zoneURL = Sys.getenv("QSI_ZONE_URL")) {
+
+  script <- "var id='INTERCEPT_ID';
+  c='INTERCEPT_ID_container';
+  var o=document.getElementById(c);
+  if(o) { o.innerHTML='';var d=o; } else { var d=document.createElement('div'); d.id=c; }
+
+  var s=document.createElement('script');
+  s.type='text/javascript';
+  s.src='https://QSI_ZONE_URL/WRSiteInterceptEngine/?Q_SIID=INTERCEPT_ID&Q_VERSION=0&Q_LOC=\"encodeURIComponent(window.location.href)\"&Q_BOOKMARKLET';
+  if(document.body){document.body.appendChild(s);document.body.appendChild(d);}
+  console.log('QSI run');"
+
+  script <- gsub("QSI_ZONE_URL", zoneURL, script)
+  gsub("INTERCEPT_ID", interceptId, script)
+}
+
 
