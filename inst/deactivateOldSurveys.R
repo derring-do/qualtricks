a49c2e99d0c6aea8d776befe3833041f7b1fa0c5
Amanda Ludden <5824530+derring-do@users.noreply.github.com>
2019-05-20 17:43:11 -0400

    edit deactivate

--- inst/deactivateOldSurveys.R
+++ inst/deactivateOldSurveys.R
@@ -7,7 +7,8 @@ all <- all_surveys()
 
 to_deactivate <- all %>%
   mutate(lastMod = as.Date(lastModified)) %>% 
-  filter(isActive == TRUE & ownerId == Sys.getenv("QUALTRICS_USERID") & lastMod == "2019-01-14") 
+  # filter(isActive == TRUE & ownerId == Sys.getenv("QUALTRICS_USERID") & lastMod == "2019-01-14") %>%
+  # filter(isActive==TRUE & ownerId != Sys.getenv("QUALTRICS_USERID"))
 
 # updateSurvey(to_deactivate$id[1], isActive=FALSE)
 
