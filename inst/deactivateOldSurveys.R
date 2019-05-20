199dcbbf160ba69aa1ebaa10f23890a040d1050c
Amanda Ludden <5824530+derring-do@users.noreply.github.com>
2019-05-20 17:33:48 -0400

    script to deactivate surveys

---
+++ inst/deactivateOldSurveys.R
@@ -0,0 +1,15 @@
+library(qualtRics)
+library(qualtricks)
+library(dplyr)
+library(purrr)
+
+all <- all_surveys()
+
+to_deactivate <- all %>%
+  mutate(lastMod = as.Date(lastModified)) %>% 
+  filter(isActive == TRUE & ownerId == Sys.getenv("QUALTRICS_USERID") & lastMod == "2019-01-14") 
+
+# updateSurvey(to_deactivate$id[1], isActive=FALSE)
+
+to_deactivate$id %>% map(function(x) {updateSurvey(x, isActive=FALSE)})
+
