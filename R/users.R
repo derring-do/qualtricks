af179ff4c6568b1d7ca96d10ec040700215efe48
Ludden <5824530+derring-do@users.noreply.github.com>
2020-02-06 14:41:33 -0500

    add offset arg to listUsers to get paged results

--- R/users.R
+++ R/users.R
@@ -1,21 +1,27 @@
 #' List Users
-#' https://api.qualtrics.com/reference/list-users
+#' https://api.qualtrics.com/docs/managing-users#list-users
 #'
 #' @param yourdatacenterid Set in .Renviron
 #' @param yourapitoken Set in .Renviron
 #' @param responseOnly Return response() object only? Defaults to FALSE.
 #'
-#' @return Response
+#' @return Response or data.frame
 #' @import httr glue jsonlite
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' listUsers()
-#' }
-listUsers <- function(yourdatacenterid = Sys.getenv("QUALTRICS_DATACENTERID"), yourapitoken = Sys.getenv("QSI_TOKEN"), responseOnly = FALSE) {
-  r <- GET(url = glue::glue("https://{yourdatacenterid}/API/v3/users"), add_headers("X-API-TOKEN" = yourapitoken))
-  
+#' listUsers(offset=100)
+#' lapply(seq(0,600,100), function(x) { listUsers(offset=x) }) %>% rbindlist
+listUsers <- function(yourdatacenterid = Sys.getenv("QUALTRICS_DATACENTERID"),
+                      yourapitoken = Sys.getenv("QSI_TOKEN"),
+                      offset = 0,
+                      responseOnly = FALSE) {
+  r <- GET(url = glue::glue("https://{yourdatacenterid}/API/v3/users"),
+           query = list(offset = offset),
+           add_headers("X-API-TOKEN" = yourapitoken))
+
   if(responseOnly) {
     return(r)
   } else {
