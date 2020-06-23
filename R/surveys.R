87f78b53ffe43bca071bae50d25042690beed3df
Ludden <5824530+derring-do@users.noreply.github.com>
2020-06-23 17:46:39 -0400

    example fxn to iterate listSurveys

--- R/surveys.R
+++ R/surveys.R
@@ -7,12 +7,17 @@
 #' @param responseOnly Return response() object only? Defaults to FALSE.
 #'
 #' @return Response or data.frame
-#' @import httr glue jsonlite
+#' @import httr glue jsonlite dplyr
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' listSurveys()
+#' a <- listSurveys()
+#' a
+#' dim(a)
+#' b <- lapply(seq(0,1000,100), function(x) { listSurveys(offset=x) }) %>% bind_rows
+#' dim(b)
+#' }
 listSurveys <- function(yourdatacenterid = Sys.getenv("QUALTRICS_DATACENTERID"),
                       yourapitoken = Sys.getenv("QSI_TOKEN"),
                       offset = 0,
