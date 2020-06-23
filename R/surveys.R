c9a2b683391d4b0d13c89725dc10c35f2ef0a5c6
Ludden <5824530+derring-do@users.noreply.github.com>
2020-06-23 17:24:50 -0400

    add listSurveys

---
+++ R/surveys.R
@@ -0,0 +1,30 @@
+#' List Surveys
+#' https://api.qualtrics.com/guides/reference/surveys.json/paths/~1surveys/get
+#'
+#' @param yourdatacenterid Set in .Renviron
+#' @param yourapitoken Set in .Renviron
+#' @param offset paging, defaults to 0 (first page only)
+#' @param responseOnly Return response() object only? Defaults to FALSE.
+#'
+#' @return Response or data.frame
+#' @import httr glue jsonlite
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' listSurveys()
+listSurveys <- function(yourdatacenterid = Sys.getenv("QUALTRICS_DATACENTERID"),
+                      yourapitoken = Sys.getenv("QSI_TOKEN"),
+                      offset = 0,
+                      responseOnly = FALSE) {
+
+  r <- GET(url = glue::glue("https://{yourdatacenterid}/API/v3/surveys"),
+           query = list(offset = offset),
+           add_headers("X-API-TOKEN" = yourapitoken))
+
+  if(responseOnly) {
+    return(r)
+  } else {
+    return(r %>% content("text") %>% fromJSON(simplifyVector = FALSE, simplifyDataFrame = TRUE) %>% .$result %>% .$elements)
+  }
+}
